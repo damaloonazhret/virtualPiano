20deaafe9f396e49df750c919f4886abcc093172
Andrey Mironov <poiprizamoda@gmail.com>
2023-03-02 20:17:02 +0300

    pianofinal

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,11 +2,31 @@ const audio = new Audio('../images/audio/a.mp3');
 const buttons = document.querySelectorAll('.piano-key');
 const html = document.querySelector('html');
 const pianoBlock = document.querySelector('.piano');
+const btnses = document.querySelectorAll('.btn');
+
+const massa = [];
+
+buttons.forEach(el => {
+    massa.push(el.getAttribute('data-key'));
+});
+
+
+btnses[1].addEventListener('click', function () {
+    buttons.forEach(el => {
+        el.classList.add('active-letter');
+        btnses[1].classList.add('btn-active');
+        btnses[0].classList.remove('btn-active');
+    });
+});
+btnses[0].addEventListener('click', function () {
+    buttons.forEach(el => {
+        el.classList.remove('active-letter');
+        btnses[0].classList.add('btn-active');
+        btnses[1].classList.remove('btn-active');
+    });
+});
 
 function playNote(e) {
-    // console.log(e.target);
-    // const key = document.querySelector(`.piano-key[data-key="${e.target.dataset.key}"]`);
-    // const audioKey = document.querySelector(`audio[data-key="${e.target.dataset.key}"]`);
     const key = document.querySelector(`.piano-key[data-key="${e}"]`);
     const audioKey = document.querySelector(`audio[data-key="${e}"]`);
     const keyNote = key.getAttribute("data-note");
@@ -14,29 +34,26 @@ function playNote(e) {
     key.classList.add('piano-key-active');
     audioKey.currentTime = 0;
     audioKey.play();
-    // console.log(key);
-    // console.log(audioKey);
-    // console.log(keyNote);
-    // console.log(audioNote);
-
-
 }
 
-function activeBtn(e) {
-    if (e == undefined) {
+
+const activeBtn = (e) => {
+    // console.log(e.target);
+    if (e == undefined || e == null) {
         return;
     }
-    // console.log(e);
     const key = document.querySelector(`.piano-key[data-key="${e}"]`);
     key.classList.remove('piano-key-active');
-}
+
+    // const key = e.target;
+    key.classList.remove('piano-key-active');
+};
 
 let mouseButton = '';
 
-function playMusic(e) {
+const playMusic = (e) => {
     buttons.forEach(el => {
         if (e.target === el) {
-            // console.log(e.target.dataset.key);
             switch (e.target.dataset.key) {
                 case e.target.dataset.key: {
                     if (mouseButton !== e.target.dataset.key) {
@@ -48,26 +65,28 @@ function playMusic(e) {
             }
         }
     });
-}
+};
 
-pianoBlock.addEventListener('mousedown', function () {
-    pianoBlock.addEventListener('mousemove', playMusic);
+pianoBlock.addEventListener('mousedown', function (e) {
+    pianoBlock.addEventListener('mouseover', playMusic);
+    activeBtn(e.target.dataset.key);
+    playMusic(e);
+    mouseButton = '';
 });
 
-pianoBlock.addEventListener('click', function () {
-    pianoBlock.addEventListener('click', playMusic);
-});
 
 html.addEventListener('mouseup', e => {
-    pianoBlock.removeEventListener('mousemove', playMusic);
-    // activeBtn(e.target.dataset.key);
-    mouseButton = '';
-});
-pianoBlock.addEventListener('mouseout', e => {
-    // window.removeEventListener('mousemove', playMusic);
-    mouseButton = '';
     activeBtn(e.target.dataset.key);
+    pianoBlock.removeEventListener('mouseover', playMusic);
+    mouseButton = '';
 });
+
+pianoBlock.addEventListener('mouseout', e => {
+    activeBtn(e.target.dataset.key);
+    mouseButton = '';
+});
+
+
 
 document.addEventListener('click', function (event) {
     if (!event.target.hasAttribute('data-toggle-fullscreen')) return;
@@ -82,18 +101,31 @@ document.addEventListener('click', function (event) {
 
 let pressingButton = '';
 html.addEventListener('keydown', e => {
-    switch (event.keyCode) {
-        case event.keyCode: {
-            if (pressingButton !== event.keyCode) {
-                pressingButton = event.keyCode;
-                playNote(pressingButton);
+    massa.forEach(el => {
+        if (el == e.keyCode) {
+            switch (event.keyCode) {
+                case event.keyCode: {
+                    if (pressingButton == null) {
+                        return;
+                    }
+                    if (pressingButton !== event.keyCode) {
+                        pressingButton = event.keyCode;
+                        playNote(pressingButton);
+                    }
+                    break;
+                }
             }
-            break;
         }
-    }
+    });
+
 });
 document.addEventListener('keyup', e => {
     pressingButton = '';
+    massa.forEach(el => {
+        if (el == e.keyCode) {
+            activeBtn(e.keyCode);
+        }
+    });
 });
 
 
